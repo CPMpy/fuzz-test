874bb9e87dabc1141a206ddfd5197d64e4298e16
MilanPesa <55391524+MilanPesa@users.noreply.github.com>
2024-07-22 13:07:33 +0200

    added cpmpy solvers + rewrite args

--- fuzz_test.py
+++ fuzz_test.py
@@ -5,6 +5,7 @@ import os
 from pathlib import Path
 from os.path import join
 import time
+from cpmpy import *
 import sys
 sys.path.append('../cpmpy')
 
@@ -25,7 +26,7 @@ def create_output_text(test_results: dict) -> str:
     """
         
     test_result_string = "Fuzz Test output: \nTest Parameters: \n\t\tTested the models: " + test_results["info"]["models"] + "\n\t\tUsed solver: " +  test_results["info"]["solver"] + " \n\t\tRandom seed: " + str(test_results["info"]["seed"]) +"\n"
-    test_result_string += "\t\tFuzz test parameters: permutations=" + str(test_results["info"]["permutations"]) + ", skip_global_constraints="+str(test_results["info"]["skip_global_constraints"])
+    test_result_string += "\t\tFuzz test parameters: mutations_per_model=" + str(test_results["info"]["mutations_per_model"]) + ", skip_global_constraints="+str(test_results["info"]["skip_global_constraints"])
     test_result_string += "\n\nExecuted " + str(test_results["info"]["executed_tests"]) + " tests in " + str(test_results["info"]["execution_time"]) + " minutes,  " + str(test_results["info"]["passed_tests"]) + " tests passed " +  str(test_results["info"]["failed_tests"]) + " tests failed"
     for key, value in test_results.items():
         if not (key == "info"):
@@ -54,7 +55,7 @@ def create_output_text(test_results: dict) -> str:
     return test_result_string
 
 '''writing the data every second, so we will not loose any date if the program should crash or gets closed'''
-def write_test_data(lock, output_dir: str, test_results: dict, start_time: float, current_amount_of_tests, current_amount_of_error, solver: str, models: list, permutations: int, skip_global_constraints: bool, rseed: int) -> None:
+def write_test_data(lock, output_dir: str, test_results: dict, start_time: float, current_amount_of_tests, current_amount_of_error, solver: str, models: list, mutations_per_model: int, skip_global_constraints: bool, rseed: int) -> None:
     """
     A helper function which will read the test data every second and write it as a txt and a pickle file
 
@@ -67,7 +68,7 @@ def write_test_data(lock, output_dir: str, test_results: dict, start_time: float
         current_amount_of_error (Value(int)): the value that stores the current amount of errors found
         solver (string): the name of the solver that is getting used for the tests
         models ([string]): the directories of the models that we are testing
-        permutations (int): the amount of permutations that we are using for the tests
+        mutations_per_model (int): the amount of mutations_per_model that we are using for the tests
         skip_global_constraints (bool): the value of skipping the global constraints
         rseed (int): the random seed that is getting used for the tests
     """
@@ -75,12 +76,13 @@ def write_test_data(lock, output_dir: str, test_results: dict, start_time: float
         while True:
             lock.acquire()
             try:
-                test_results["info"] = {"execution_time":math.floor((time.time()-start_time)/60), "executed_tests": current_amount_of_tests.value, "passed_tests": current_amount_of_tests.value-current_amount_of_error.value, "failed_tests": current_amount_of_error.value,"solver":solver,"models": models,"permutations": permutations, "skip_global_constraints": skip_global_constraints, "seed":rseed }
+                test_results["info"] = {"execution_time":math.floor((time.time()-start_time)/60), "executed_tests": current_amount_of_tests.value, "passed_tests": current_amount_of_tests.value-current_amount_of_error.value, "failed_tests": current_amount_of_error.value,"solver":solver,"models": models,"mutations_per_model": mutations_per_model, "skip_global_constraints": skip_global_constraints, "seed":rseed }
                 
-                with open(join(output_dir, 'output'), "wb") as ff:
-                    pickle.dump(test_results, file=ff)  # log some stats
+                with open(join(output_dir, 'run_fuzz_test_output.pickle'), "wb") as ff:
+                    # use copy since the normal test results is a multiprocessing.managers.DictProxy while we need a normal dict
+                    pickle.dump(test_results.copy(), file=ff) 
 
-                with open(join(output_dir, 'output.txt'), "w") as ff:
+                with open(join(output_dir, 'run_fuzz_test_output.txt'), "w") as ff:
                     ff.write(create_output_text(test_results))
             finally:
                 lock.release()  
@@ -108,44 +110,56 @@ def time_out_process(mins : int, current_amount_of_error, max_error_treshold: in
     except KeyboardInterrupt:
         print("interrupting...")
     finally: 
-        print("\n executed tests for "+str(math.floor((time.time()-start_time)/60))+" minutes writing the data...")
+        print("\n executed tests for "+str(math.floor((time.time()-start_time)/60))+" minutes")
 
 
 
 if __name__ == '__main__':
-    # Getting and checking the input parameters
-
-    available_solvers = ["ortools","z3"]
+    # Getting and checking the input parameters    
+    def getsolvernames(solver) -> str:
+        """
+        Small helper function for getting al the available solvers names from cpmpy
+        """
+        return solver[0]
+    
+    # get all the available solvers from cpympy
+    available_solvers = list(map(getsolvernames, SolverLookup.base_solvers()))
 
     def check_positive(value):
+        """
+        Small helper function used in the argparser for checking if the input values are positive or not
+        """
         ivalue = int(value)
         if ivalue <= 0:
             raise argparse.ArgumentTypeError("%s is an invalid positive int value" % value)
         return ivalue
-
+    
     parser = argparse.ArgumentParser(description = "A python application to fuzz_test your solver(s)")
-    parser.add_argument("-H", "--Help", help = "To use fuzz test give: the params --solver", required=False, default = "")
-    parser.add_argument("-s", "--solver", help = "The Solver to use", required = True,type=str,choices=available_solvers)
-    parser.add_argument("-m", "--models", help = "The path to load the models", required=True, type=str)
-    parser.add_argument("-o", "--output_dir", help = "The directory to store the output", required=True)
-    parser.add_argument("-g", "--skip_global_constraints", help = "Skip the global constraints when testing", required=False, default = False)
-    parser.add_argument("-a", "--bug_treshold", help = "The bug treshold when to quit", required=False, default=0 ,type=check_positive)
-    parser.add_argument("--minutes", help = "The time in minutes to run the tests", required=False, default=1 ,type=check_positive)
-    parser.add_argument("--permutations", help = "The amount of permutations", required=False, default=5 ,type=check_positive)
+    parser.add_argument("-s", "--solver", help = "The Solver to use", required = False,type=str,choices=available_solvers, default=available_solvers[0])
+    parser.add_argument("-m", "--models", help = "The path to load the models", required=False, type=str, default="models")
+    parser.add_argument("-o", "--output-dir", help = "The directory to store the output (will be created if)", required=False, type=str, default="output")
+    parser.add_argument("-g", "--skip-global-constraints", help = "Skip the global constraints when testing", required=False, default = False)
+    parser.add_argument("--max-bugs", help = "The bug treshold when to quit", required=False, default=0 ,type=check_positive)
+    parser.add_argument("--max-minutes", help = "The time in minutes to run the tests", required=False, default=0 ,type=check_positive)
+    parser.add_argument("-mpm","--mutations-per-model", help = "The amount of mutations that be executed on every model", required=False, default=5 ,type=check_positive)
 
 
     args = parser.parse_args()
-    models = str.split(args.models,",")
+    models = []
+    max_error_treshold = args.max_bugs
+    max_minutes = args.max_minutes
 
-    max_error_treshold = args.bug_treshold
     # if no max error treshold is given set it to inf
     if max_error_treshold == 0:
         max_error_treshold = math.inf
 
-    # check if all the model dirs exists
-    for model in models:
-        if not Path(model).exists():
-            raise ValueError("path to "+model+" model doesn't exist, please check if the path is correct")
+    # if no max minutes treshold is given set it to inf
+    if max_minutes == 0:
+        max_minutes = math.inf
+
+    # create a list with all the directories
+    for model in os.listdir(args.models):
+        models.append(os.path.join(args.models, model))
 
     # create the output dir if it does not yet exists
     if not Path(args.output_dir).exists():
@@ -166,7 +180,7 @@ if __name__ == '__main__':
     # creating processes to run all the tests
 
     processes = []
-    process_args = (test_results,current_amount_of_tests, current_amount_of_error, lock, args.solver, args.permutations ,models ,max_error_treshold,rseed)
+    process_args = (test_results,current_amount_of_tests, current_amount_of_error, lock, args.solver, args.mutations_per_model ,models ,max_error_treshold,rseed)
     
     processes.append(Process(target=solution_tests, args=process_args))
     processes.append(Process(target=optimization_tests, args=process_args))
@@ -174,9 +188,9 @@ if __name__ == '__main__':
     processes.append(Process(target=equivalance_tests,args=process_args))
     processes.append(Process(target=metamorphic_tests,args=process_args))
 
-    write_test_data_process = Process(target=write_test_data,args=(lock,args.output_dir,test_results,start_time,current_amount_of_tests,current_amount_of_error,args.solver,args.models,args.permutations, args.skip_global_constraints,rseed))
+    write_test_data_process = Process(target=write_test_data,args=(lock,args.output_dir,test_results,start_time,current_amount_of_tests,current_amount_of_error,args.solver,args.models,args.mutations_per_model, args.skip_global_constraints,rseed))
 
-    timing_process = Process(target=time_out_process,args=(args.minutes,current_amount_of_error,max_error_treshold,start_time))
+    timing_process = Process(target=time_out_process,args=(max_minutes,current_amount_of_error,max_error_treshold,start_time))
     
     
     # start the processes
