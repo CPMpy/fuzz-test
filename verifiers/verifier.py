4b296e34f8bfc38aef683e510a277c0b89a6ea2a
MilanPesa <55391524+MilanPesa@users.noreply.github.com>
2024-09-20 16:32:03 +0200

    not skipping the transformations

--- verifiers/verifier.py
+++ verifiers/verifier.py
@@ -151,8 +151,15 @@ class Verifier():
             self.original_model = error["originalmodel"]
             self.exclude_dict = {}
             self.initialize_run()
-            self.cons = error["constraints"]
-            return self.verify_model()
+            gen_mutations_error = self.generate_mutations()
+
+            # check if no error occured while generation the mutations
+            if gen_mutations_error == None:
+                return self.verify_model()
+            else:
+                return gen_mutations_error
+            # self.cons = error["constraints"]
+            # return self.verify_model()
         
         except AssertionError as e:
             print("A", end='',flush=True)
