e3aaf904bd65f307b1086d3ef9ba8f93b5318210
wout4 <wout.vanroose@gmail.com>
2024-12-02 15:11:38 +0100

    time 10 iterations

--- benchmark_transform.py
+++ benchmark_transform.py
@@ -27,7 +27,7 @@ for folder in models:
     fmodels.extend(glob.glob(join(folder, 'sat', "*")))
 
 cons = []
-for modfile in fmodels:
+for modfile in fmodels[:5]:
     with open(modfile, 'rb') as fpcl:
         cons.extend(pickle.loads(fpcl.read()).constraints)
 
@@ -35,6 +35,19 @@ for modfile in fmodels:
 from cpmpy.solvers import CPM_ortools
 
 # Time the transformation of constraints
-transform_time = timeit.timeit(lambda: CPM_ortools.transform(CPM_ortools(), cons), number=1)
-print(transform_time)
+times = []
+for _ in range(10):
+    # Reset cons for each run, otherwise _has_subexpr is already initialised, and would give more benifit than is realistic
+    cons = []
+    for modfile in fmodels[:5]:
+        with open(modfile, 'rb') as fpcl:
+            cons.extend(pickle.loads(fpcl.read()).constraints)
+    
+    # Time single transformation
+    transform_time = timeit.timeit(lambda: CPM_ortools.transform(CPM_ortools(), cons), number=1)
+    times.append(transform_time)
 
+transform_time = sum(times) / len(times)
+print(transform_time)
+print(times)
+print("Standard deviation:", (sum((x - transform_time) ** 2 for x in times) / len(times)) ** 0.5)
